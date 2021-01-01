e509b3e3137ea498f2728d4ddc5c24aebfde6d0c
kwatme <kwat.me@icloud.com>
2020-12-31 17:33:32 -0800

    merge_recursively

--- src/extend_layout.jl
+++ src/extend_layout.jl
@@ -1,9 +1,12 @@
 using Plotly: Layout, attr
 
+using Kraft: merge_recursively
+
 function extend_layout(layout; kwargs...)
+
     new_layout = Layout()
 
-    new_layout.fields = attr(layout.fields; kwargs...).fields
+    new_layout.fields = merge_recursively(layout.fields, attr(; kwargs...).fields)
 
     return new_layout
 end
