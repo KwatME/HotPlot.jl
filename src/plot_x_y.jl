84ea179b66cf7419f187653f20cec0ddf317d315
KwatME <kwat.me@icloud.com>
2021-05-01 20:53:47 -0700

    :)

--- src/plot_x_y.jl
+++ src/plot_x_y.jl
@@ -1,21 +1,19 @@
 using Plotly: Layout, plot, scatter
 
 function plot_x_y(
-    x_::Tuple{Vararg{Vector{Float64}}},
-    y_::Tuple{Vararg{Vector{Float64}}};
-    name_::Union{Nothing, Tuple{Vararg{String}}} = nothing,
+    x_::Vector{Vector{Float64}},
+    y_::Vector{Vector{Float64}};
+    text_::Union{Nothing, Vector{Vector{String}}} = nothing,
+    name_::Union{Nothing, Vector{String}} = nothing,
+    mode_::Union{Nothing, Vector{String}} = nothing,
     layout::Union{Nothing, Layout} = nothing,
 )::Any
 
+    println(typeof(x_))
+
     n = length(x_)
 
-    if name_ === nothing
-
-        name_ = Tuple(string(i) for i in 1:n)
-
-    end
-
-    trace_ = Tuple(Dict{String, Any}() for i in 1:n)
+    trace_ = [Dict{String, Any}() for i in 1:n]
 
     for i in 1:n
 
@@ -23,13 +21,57 @@ function plot_x_y(
 
         trace_[i]["y"] = y_[i]
 
-        trace_[i]["name"] = name_[i]
+        if text_ == nothing
+
+            text = nothing
+
+        else
+
+            text = text_[i]
+
+        end
+
+        trace_[i]["text"] = text
+
+        if name_ == nothing
+
+            name = string(i)
+
+        else
+
+            name = name_[i]
+
+        end
+
+        trace_[i]["name"] = name
+
+        if mode_ == nothing
+
+            if length(x_[i]) < 1000
+
+                mode = "markers"
+
+            else
+
+                mode = "lines"
+
+            end
+
+        else
+
+            mode = mode_[i]
+
+        end
+
+        trace_[i]["mode"] = mode
+
+        trace_[i]["opacity"] = 0.8
 
     end
 
     trace_ = [scatter(trace) for trace in trace_]
 
-    if layout === nothing
+    if layout == nothing
 
         layout = Layout()
 
@@ -39,9 +81,9 @@ function plot_x_y(
 
 end
 
-function plot_x_y(y_::Tuple{Vararg{Vector{Float64}}}; kwargs...)::Any
+function plot_x_y(y_::Vector{Vector{Float64}}; kwargs...)::Any
 
-    return plot_x_y(Tuple(Float64.(1:length(y)) for y in y_), y_; kwargs...)
+    return plot_x_y([Float64.(1:length(y)) for y in y_], y_; kwargs...)
 
 end
 
--- src/use_style!.jl
+++ src/use_style!.jl
@@ -5,7 +5,13 @@ import Plotly: use_style!
 function use_style!()::Style
 
     return use_style!(
-        Style(layout = Layout(autosize = false, template = "plotly_white")),
+        Style(
+            layout = Layout(
+                autosize = false,
+                template = "plotly_white",
+                hovermode = "closest",
+            ),
+        ),
     )
 
 end
